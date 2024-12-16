4c26d9273bb83be315dff38f9f8d88d4000774eb
Danilo Ruan <daniloruan5@gmail.com>
2024-12-16 07:45:56 -0300

    sistema de baixar videos do youtube

---
+++ func_download.py
@@ -0,0 +1,19 @@
+from pytubefix import YouTube
+
+def download_vid(url):
+
+    #pegando o valor da url
+    value_url = url
+
+    #formatando para texto
+    form_url = str(value_url)
+
+    #baixando o video/audio
+    donwload = YouTube(form_url)
+
+    #escolhendo resolucao
+
+    donwload_format = donwload.streams.filter(progressive=True, file_extension="mp4", res="1080p").first()
+
+    donwload_video = donwload_format.download()
+
---
+++ yt_interface.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from func_download import *
+
+
+
+
+# janela principal
+def janela():
+
+    #fucao para fazer o download do video/audio
+    def enviar():
+        valor_url = url.get()
+
+        download_vid(url=valor_url)
+        print(f"A url inserida e: {valor_url}")
+
+        #testando lista
+    
+    #iniciando interface
+    janela = Tk()
+    janela.title("Youtube Downloader")
+    janela.geometry("400x300")
+
+
+    #titulo
+    titulo = Label(font=("arial", 20, "bold"), text="Coloque sua URL")
+    titulo.pack()
+
+    #valor para introduzir a URL
+    url = Entry(janela, font=("arial", 12), width=10)
+    url.pack()
+
+    #escolhei a resolucao
+
+    #botao de enviar
+    download = Button(janela, text="download", command=enviar)
+    download.pack()
+
+
+    janela.mainloop()
+
+janela()
+
